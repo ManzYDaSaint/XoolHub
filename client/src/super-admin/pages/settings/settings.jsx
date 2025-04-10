2ad94293fa5cc0eb4404ead9d51f08db22b007c2
ManzYDaSaint <ManzyN@outlook.com>
2025-04-10 05:58:01 +0200

    update

--- client/src/super-admin/pages/settings/settings.jsx
+++ client/src/super-admin/pages/settings/settings.jsx
@@ -15,29 +15,28 @@ const Setting = () => {
           <div className="dashboard">
             <Menu />
             <div className="settingContainer">
-            <div className="flex bg-gray-100 pb-3">
-                  <div className="flex-1 flex flex-col">
-                    {/* Header */}
-                    <div className="mb-1 flex flex-col sm:flex-row sm:items-center sm:justify-between shadow p-4">
-                      <div>
-                        <h1
-                          className="text-lg font-semibold"
-                          style={{ fontFamily: "'Poppins', san-serif" }}
-                        >
-                          Settings
-                        </h1>
-                        <p className="mt-1 text-sm text-gray-500">
-                          Manage the whole application settings.
-                        </p>
-                      </div>
-                      <div className="mt-4 sm:mt-0"></div>
+              <div className="flex bg-gray-100 pb-3">
+                <div className="flex-1 flex flex-col">
+                  {/* Header */}
+                  <div className="mb-1 flex flex-col sm:flex-row sm:items-center sm:justify-between shadow p-4">
+                    <div>
+                      <h1
+                        className="text-lg font-semibold"
+                        style={{ fontFamily: "'Poppins', san-serif" }}
+                      >
+                        Settings
+                      </h1>
+                      <p className="mt-1 text-sm text-gray-500">
+                        Manage the whole application settings.
+                      </p>
                     </div>
-                    <div className="mt-10 px-5">
-
-              <Tabs />
-                    </div>
+                    <div className="mt-4 sm:mt-0"></div>
+                  </div>
+                  <div className="mt-10 px-5">
+                    <Tabs />
+                  </div>
+                </div>
               </div>
-                    </div>
             </div>
           </div>
         </div>
